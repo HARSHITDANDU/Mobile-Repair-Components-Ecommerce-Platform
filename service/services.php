9116a7cdfda2986a6d04b8984341be5a095789e4
HARSHITDANDU <142598007+HARSHITDANDU@users.noreply.github.com>
2025-08-22 22:03:51 +0530

    Add files via upload

---
+++ service/services.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+include '../connect.php';
+include '../pagination.php';
+
+function getTotalService(){
+    include '../connect.php';
+    $sql = "SELECT COUNT(*) AS count FROM services";
+    $result = mysqli_query($con,$sql);
+    $row = mysqli_fetch_assoc($result);
+    $count = $row['count'];
+
+    return $count;
+ }
+
+ function getServices($offset,$itemsPerPage){
+    include "../connect.php";
+
+        $services_query = "
+            SELECT s.service_id,s.service_name, s.description, s.price, s.image_link, sc.name AS category_name
+            FROM services s
+            JOIN service_catogory sc ON s.catogory_id = sc.service_cat_id
+            LIMIT $itemsPerPage OFFSET $offset
+        ";
+
+
+        $services_result = $con->query($services_query);
+
+        $service_list = array();
+        while ($row = mysqli_fetch_assoc($services_result)) {
+            array_push($service_list, $row);
+        }
+
+        return $service_list;
+
+
+ }
+$service_category_query = "SELECT service_cat_id, name FROM service_catogory";
+$service_category_result = $con->query($service_category_query);
+$service_category_list = array();
+while ($row = mysqli_fetch_assoc($service_category_result)) {
+    $service_category_list[] = $row;
+}
+
+
+$search_value = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['search_value'])) {
+        $search_value = $_POST['search_value'];
+    }
+}
+
+$cat_id = "none";
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+   
+    if (isset($_GET['cat_id'])) {
+        $cat_id = $_GET['cat_id'];
+       
+    }
+}
+
+$totalProducts = getTotalService(); 
+$itemsPerPage = 12; 
+$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($currentPage - 1) * $itemsPerPage;
+$service_list = getServices($offset, $itemsPerPage);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>products</title>
+    <link rel="stylesheet" href="../style/services.css">
+</head>
+
+<body>
+<div class="header">
+        <div class="nav-bar-logo">
+            <a href="../Home/dashbord.php">
+            <img src="../images/elife_logo.png" width="140" height="70">
+            </a>
+        </div>
+        <div class="category">
+            <select name="category" id="category" onchange="categorySearch()">
+                <option value="<?php echo $cat_id; ?>">All Categories</option>
+                <?php foreach ($service_category_list as $category): ?>
+                    <option value="<?php echo $category['service_cat_id']; ?>"><?php echo $category['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="search-container">
+            <form action="javascript:void(0);" class="search">
+                <input type="text" id="search" placeholder="Search service..." value="<?php echo $search_value?>" />
+                <button id="search-btn" type="button" onclick="searchServices()">Search</button>
+            </form>
+        </div>
+        
+    </div>
+
+
+    <div class="product-section-container" id="product-section-container">
+
+        <ul class="product-section-item-wrapper">
+            <?php foreach ($service_list as $service): ?>
+                <a class="service-link" href="serviceviewpage.php?service_id='<?php echo $service['service_id']; ?>'"><li class="product-item">
+                    <div class="product-image">
+                        <img src="../images/<?php echo $service['image_link'] ?>" alt="smart watch">
+
+                    </div>
+                    <div class="product-text">
+                        <div class="service-name">
+                            <span class="product-title">
+                                <?php echo $service['service_name'] ?>
+                            </span>
+                        </div>
+                        
+                        <div class="product-purchace">
+                            <span class="product-price">
+                                <?php echo "$" . $service['price'] ?>
+                            </span>
+                            <!-- Todo need to implement serviceview page -->
+                            
+
+                        </div>
+
+                    </div>
+
+                </li></a>
+
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <?php echo paginate($totalProducts, $itemsPerPage, $currentPage, 'services.php'); ?>
+
+
+<script src="../script/serviceSearch.js">
+        
+
+</script>
+
+
+
+</body>
+
+</html>
---
+++ service/storeservicerequest.php
@@ -0,0 +1,79 @@
+<?php 
+include "../connect.php";
+session_start();
+
+$sucuss = 0;
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+    $description = $_POST['description'];
+    $service_id = $_POST['service_id'];
+    $contact_number = $_POST['contact_number'];
+    $user_id = $_SESSION['userid'];
+
+    $sql = "INSERT INTO service_requests (service_id, user_id, location, user_name, description, contact_number) 
+    VALUES ('$service_id', '$user_id', '$location', '$name', '$description', '$contact_number')";
+
+$result = mysqli_query($con,$sql);
+
+if($result){
+    $sucuss =1;
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            position: relative;
+        }
+        .container{
+            position: absolute;
+            top: 25vh;
+            left: 30%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .back{
+            border: none;
+            background-color: black;
+            padding: 10px;
+            width: 150px;
+            color: white;
+            border-radius: 7px;
+            cursor: pointer;
+            font-size: 14px;
+            margin-top: 15px;
+            
+        }
+        .back:hover{
+            opacity: 0.9;
+        }
+    </style>
+</head>
+<body>
+    <?php if($sucuss==1): ?>
+        <div class="container">
+            <h1>Service Provider will contact you soon.</h1>
+            <img src="../images/contact.png" width="200px" height="160px">
+            <a href="../Home/dashbord.php"><button class="back">Back to home</button></a>
+        </div>
+        
+   
+    <?php else: ?>
+    <h1>Oops! Some error has occured.Please try again.</h1>
+    <?php endif; ?>
+
+    <script>
+        setTimeout(function() {
+    window.location.href = "../userservicerequest/userservicerequest.php"; // Replace with your target URL
+}, 5000);
+    </script>
+</body>
+</html>
